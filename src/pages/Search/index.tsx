aab385af5910a6a918e28bab418b8374304a632f
Ani-21 <kvikviniya.am@edu.spbstu.ru>
2023-01-09 13:00:51 +0300

    fix/search_icon

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -5,7 +5,6 @@ import SearchBar from '../../components/SearchBar';
 import IMovie from '../../models/IMovie';
 import { useParams } from 'react-router-dom';
 import './index.scss';
-import Trending from '../../components/Trending/index';
 
 const Search = () => {
   const { searchTerm } = useParams<string>();
@@ -27,26 +26,20 @@ const Search = () => {
     searchQuery();
   }, [searchTerm]);
 
-  console.log(results);
-
   return (
     <div className="search_wrapper">
       <SearchBar />
-      {!searchTerm ? (
-        <Trending />
-      ) : (
-        <div className="wrapper">
-          {results?.map((item: IMovie, i: number) => (
-            <Card
-              id={item.id}
-              name={item.title}
-              key={i}
-              imgUrl={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
-              rate={item.vote_average}
-            />
-          ))}
-        </div>
-      )}
+      <div className="wrapper">
+        {results?.map((item: IMovie, i: number) => (
+          <Card
+            id={item.id}
+            name={item.title}
+            key={i}
+            imgUrl={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+            rate={item.vote_average}
+          />
+        ))}
+      </div>
     </div>
   );
 };
